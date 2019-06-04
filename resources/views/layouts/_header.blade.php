e553e5a547c3932b9b1d88441dc055ba129493cd
LY <571666271@qq.com>
2019-06-04 03:03:00 +0000

    用户话题列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -37,22 +37,22 @@
                                 </a>
                                 <div class="dropdown-divider"></div>
                             @endcan
-                            <a class="dropdown-item" href="{{  route('users.show', Auth::id()) }}">
-                                <i class="far fa-user mr-2"></i>
-                                个人中心
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
-                                <i class="far fa-edit mr-2"></i>
-                                编辑资料
-                            </a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" id="logout" href="#">
-                                <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
-                                    {{ csrf_field() }}
-                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                                </form>
-                            </a>
+                                <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
+                                    <i class="far fa-user mr-2"></i>
+                                    个人中心
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
+                                    <i class="far fa-edit mr-2"></i>
+                                    编辑资料
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" id="logout" href="#">
+                                    <form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗？');">
+                                        {{ csrf_field() }}
+                                        <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                    </form>
+                                </a>
                         </div>
                     </li>
                 @endguest
